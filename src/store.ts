8eabddba9f7b8eef7ab482d0b37da337887c6b24
Dimitri Loukine <dlo@wine-logics.com>
2023-06-25 23:39:56 +0200

    tests passed

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { JSONObject, JSONValue } from "./json-types";
+import "reflect-metadata";
 
 export type Permission = "r" | "w" | "rw" | "none";
 
@@ -12,31 +13,120 @@ export interface IStore {
   entries(): JSONObject;
 }
 
-export function Restrict(...params: any): any {
-  throw new Error("Method not implemented.");
+export type RestrictedProperty = {
+  key: string;
+  readable: boolean;
+  writable: boolean;
 }
 
+export function Restrict(permission?:Permission): any  {
+  return (target: Store, propertyKey: string ) => { 
+    let value:any;
+    // TODO  default permissions
+    // const permission:Permission = assignedPermission || Object.getOwnPropertyDescriptor(target, 'defaultPolicy')!.value;
+    const readable:boolean = permission == 'r' || permission == "rw";
+    const writable:boolean = permission == 'w' || permission == "rw";
+
+    const restrictedProperties:RestrictedProperty[] = Reflect.getMetadata ('restrictedProperties', target) || [];
+    if (!restrictedProperties.find(value => value.key == propertyKey)) {
+      restrictedProperties.push({
+        key: propertyKey,
+        readable: readable,
+        writable: writable
+      })
+    }
+
+    Reflect.defineMetadata('restrictedProperties', restrictedProperties, target)
+
+    const getter = function() {
+      return value;
+    };
+    const setter = function(newValue: any) {
+      value = newValue;
+    };
+
+    Object.defineProperty(target, propertyKey, {
+      set: setter,
+      get: getter,
+      configurable: true
+    });
+  }
+}
 export class Store implements IStore {
   defaultPolicy: Permission = "rw";
 
+  constructor () {
+
+  }
+
   allowedToRead(key: string): boolean {
-    throw new Error("Method not implemented.");
+    const restrictedProperty:RestrictedProperty = Reflect.getMetadata('restrictedProperties', this)?.find((value:RestrictedProperty) => value.key == key)
+    if(!restrictedProperty){
+      // fallback to default policy
+      return this.defaultPolicy == 'r' || this.defaultPolicy == 'rw';
+    }else{
+      return restrictedProperty.readable
+    }
   }
 
   allowedToWrite(key: string): boolean {
-    throw new Error("Method not implemented.");
+    console.log(key)
+    console.log(this.defaultPolicy)
+    const restrictedProperty:RestrictedProperty = Reflect.getOwnMetadata('restrictedProperties', this)?.find((value:RestrictedProperty) => value.key == key)
+    if(!restrictedProperty){
+      // fallback to default policy
+      return  this.defaultPolicy == 'w' || this.defaultPolicy == 'rw';
+    }else{
+      return restrictedProperty.writable
+    }
   }
 
   read(path: string): any {
-    throw new Error("Method not implemented.");
+    const segments:string[] = path.split(':', 2);
+    const firstKey:string = segments[0];
+    const remainingPath:string = segments[1];
+
+    if(segments.length > 1) {
+      if(typeof (this as any)[firstKey] === 'function'){
+        return (this as any)[firstKey]().read(remainingPath)
+      }else{
+        return (this as any)[firstKey].read(remainingPath)
+      }
+    }else {
+      if(!this.allowedToRead(firstKey)){
+        throw new Error("Read Access Denied");
+      }
+      return this[firstKey as keyof Store];
+    }
   }
 
   write(path: string, value: JSONValue): JSONValue | IStore {
-    throw new Error("Method not implemented.");
+    const segments:string[] = path.split(':', 2);
+    const firstKey:string = segments[0];
+    const remainingPath:string = segments[1];
+    if(segments.length > 1) {
+      if(((this as any)[firstKey] instanceof Store) ){
+        (this as any)[firstKey].write(remainingPath, value)
+      }else{
+        if(!this.allowedToWrite(firstKey)){
+          throw new Error("Write Access Denied");
+        }
+        (this as any)[firstKey] = new Store();
+        (this as any)[firstKey].write(remainingPath, value);
+      }
+      return (this as any)[firstKey];
+    }else {
+      if(!this.allowedToWrite(firstKey)){
+        throw new Error("Write Access Denied");
+      }
+      return (this as any)[firstKey] = value;
+    } 
   }
 
   writeEntries(entries: JSONObject): void {
-    throw new Error("Method not implemented.");
+    for (const key in entries) {
+      this.write(key, entries[key])
+    }
   }
 
   entries(): JSONObject {
--- src/userStore.ts
+++ src/userStore.ts
@@ -3,7 +3,6 @@ import { Restrict, Store } from "./store";
 export class UserStore extends Store {
   @Restrict("rw")
   name: string = "John Doe";
-
   constructor() {
     super();
     this.defaultPolicy = "rw";
